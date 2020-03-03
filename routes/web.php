7b8fc6429d561ff0c9902a7c110e57399f5fbd2a
arif <bickkuarif@gmail.com>
2020-03-04 02:10:11 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('test', function () {
         ],
         'scope'   => ['project' => ['id' => '4d9031e2761c482e873ee7fcdf73ba29']]
     ]);
-    $service = $openstack->objectStoreV1 ();
+    $service = $openstack_server->objectStoreV1();
     echo "test";
-    dd ($service->listContainers ());
+    dd ($service->listContainers());
 });
 
 Route::get('test1', function () {
