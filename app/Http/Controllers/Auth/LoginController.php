b7f72380389e4ecd4a0c7f94369225b3eb3e1a8c
arif <bickkuarif@gmail.com>
2020-03-25 20:44:28 +0530

    with ldap v4

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use LdapRecord\Laravel\Auth\ListensForLdapBindFailure;
 
 class LoginController extends Controller
 {
@@ -20,7 +21,7 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers,ListensForLdapBindFailure;
 
     /**
      * Where to redirect users after login.
@@ -37,6 +38,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->listenForLdapBindFailure();
     }
 
     // public function username(){
