572700fa17d682131894fa7b3a6d01a50eddccc7
arif <bickkuarif@gmail.com>
2020-03-29 11:04:20 +0530

    terraform script

--- app/Repository/IpaRepository.php
+++ app/Repository/IpaRepository.php
@@ -33,8 +33,8 @@ class IpaRepository
         curl_setopt($ch, CURLOPT_URL, $this->ipa_login);
         curl_setopt($ch, CURLOPT_REFERER, $this->ipa_referer);
         curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/x-www-form-urlencoded','Accept: application/json'));
-        // curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiePath);
-        // curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiePath);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiePath);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiePath);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, "user=arif&password=redhat");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -42,8 +42,8 @@ class IpaRepository
         curl_setopt($ch, CURLOPT_CAPATH, $certPath);
         // curl_setopt($ch, CURLOPT_HEADER, 0);
         $content = curl_exec($ch);
-        curl_close($ch);
-        return $content;
+        // curl_close($ch);
+        return curl_getinfo($ch, CURLINFO_HTTP_CODE);
     }
     
 }
