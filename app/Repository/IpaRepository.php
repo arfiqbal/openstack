8380dadef78d73138505bb02f7766984fa4d706c
arif <bickkuarif@gmail.com>
2020-03-29 12:15:41 +0530

    terraform script v1

--- app/Repository/IpaRepository.php
+++ app/Repository/IpaRepository.php
@@ -49,6 +49,7 @@ class IpaRepository
     public function addUser($username, $cookieName)
     {   
         $data = '{"method":"hbacrule_add","params":[["test"],{"version":"2.231"}]}';
+        $certPath =  public_path('include/ipa.ca.crt');
 
         $cookiePath =  storage_path('app/public/'.$cookieName);
         $ch = curl_init();
