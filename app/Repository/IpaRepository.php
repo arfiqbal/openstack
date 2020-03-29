fe82fab2f165c312179c3e7d87099f34f7676fdd
arif <bickkuarif@gmail.com>
2020-03-29 10:12:21 +0530

    terraform script

--- app/Repository/IpaRepository.php
+++ app/Repository/IpaRepository.php
@@ -31,15 +31,15 @@ class IpaRepository
         
         curl_setopt($ch, CURLOPT_URL, $this->ipa_login);
         curl_setopt($ch, CURLOPT_REFERER, $this->ipa_referer);
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/x-www-form-urlencoded','Accept: application/json'));
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiePath);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiePath);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->credential());
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CAINFO, $certPath);
-        curl_setopt($ch, CURLOPT_CAPATH, $certPath);
+        // curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/x-www-form-urlencoded','Accept: application/json'));
+        // curl_setopt($ch, CURLOPT_COOKIEJAR, $cookiePath);
+        // curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiePath);
+        // curl_setopt($ch, CURLOPT_POST, true);
+        // curl_setopt($ch, CURLOPT_VERBOSE, true);
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, $this->credential());
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // curl_setopt($ch, CURLOPT_CAINFO, $certPath);
+        // curl_setopt($ch, CURLOPT_CAPATH, $certPath);
         
         $content = curl_exec($ch);
         curl_close($ch);
