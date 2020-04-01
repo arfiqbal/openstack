087caad78134e1a68abfceedcc6358aacf5a2ef5
arif <bickkuarif@gmail.com>
2020-04-01 19:02:55 +0530

    configure mail

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -63,7 +63,7 @@ class VmController extends Controller
     public function create()
     {
         $users = User::where('mail', '=', 'mdarif.iqbal@vodafone.com')->get();
-        dd(Auth::user()->mail);
+        dd(Auth::user());
         Mail::to('mdarif.iqbal@vodafone.com')->send(new VmLaunched());
         
         $allVM = VM::with('application')->where('active',1)->get();
