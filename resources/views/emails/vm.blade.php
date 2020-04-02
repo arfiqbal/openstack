24a4c8ef8a61759cfd81dded64e2913ad5fb62d1
arif <bickkuarif@gmail.com>
2020-04-02 08:07:43 +0530

    configure mail

--- resources/views/emails/vm.blade.php
+++ resources/views/emails/vm.blade.php
@@ -1,106 +1,338 @@
+<!doctype html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<style type="text/css" style="display:none;"> P {margin-top:0;margin-bottom:0;} </style>
-</head>
-<body dir="ltr">
+  <head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" crossorigin="anonymous">
+    <title>Vodafone</title>
+    <style>
+      /* -------------------------------------
+          GLOBAL RESETS
+      ------------------------------------- */
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%; }
 
-<div>
-<center>
-<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="height:100%!important; margin:0; padding:20px 0 0 0; width:100%!important; background-color:#e8e8e8">
-<tbody>
-<tr>
-<td align="center" valign="top">
-<table border="0" cellpadding="10" cellspacing="0" width="600" style="background-color:#505050">
-<tbody>
-<tr>
-<td valign="top" width="20" style="color:#d8d8d8; font-family:Arial; font-size:10px; line-height:100%; text-align:center; padding:20px">
-</td>
-<td valign="top" style="color:#d8d8d8; font-family:Arial; font-size:10px; line-height:100%; text-align:left; padding-left:0px">
-<table border="0" cellpadding="10" cellspacing="0" width="100%">
-<tbody>
-<tr>
-<td valign="top" style="padding:9px 0 0 0; font-family:Arial; font-size:14px; color:#FFF">
-Cloud Notification </td>
-</tr>
-<tr>
-<td valign="top" style="padding:7px 0 0 0; font-family:Arial; font-size:11px; color:#FFF">
-vm name </td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-<table border="0" cellpadding="0" cellspacing="0" width="600" style="border:1px solid #DDDDDD; background-color:#FFFFFF">
-<tbody>
-<tr>
-<td align="center" valign="top">
-<table border="0" cellpadding="0" cellspacing="0" width="600" id="x_templateBody">
-<tbody>
-<tr>
-<td valign="top">
-<table border="0" cellpadding="20" cellspacing="0" width="100%">
-<tbody>
-<tr>
-<td valign="top" style="background-color:#FFFFFF">
-<div style="font-family:Arial; font-size:14px; line-height:150%; text-align:left">
-<h4 style="display:block; font-weight:bold; line-height:100%; margin-top:0; margin-right:0; margin-bottom:10px; margin-left:0">
-{{$vm->name}}</h4>
-<span style="font-size:12px; color:#8a8a8a">Following are the vm details </span>
-</div>
-</td>
-</tr>
-<tr>
-<td valign="top" style="padding-top:0; padding-bottom:0">
-<table border="0" cellpadding="10" cellspacing="0" width="100%" style="background-color:#FFFFFF; border:1px solid #DDDDDD">
-<tbody>
-<tr>
-<th scope="col" valign="top" width="50" style="background-color:#ebebeb; color:#434e33; font-family:Helvetica; font-size:10px; font-weight:normal; line-height:150%; text-align:right">
-Count </th>
-<th scope="col" valign="top" style="background-color:#ebebeb; color:#434e33; font-family:Helvetica; font-size:10px; font-weight:normal; line-height:150%; text-align:left">
-Event Type </th>
-</tr>
-<tr>
-<td valign="top" style="border-top:1px solid #DDDDDD; border-bottom:0; color:#646464; font-family:Helvetica; font-size:12px; line-height:150%; text-align:right">
-1 </td>
-<td valign="top" style="border-top:1px solid #DDDDDD; border-bottom:0; color:#646464; font-family:Helvetica; font-size:12px; line-height:150%; text-align:left">
-Health Rule </td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
+      body {
+        background-color: #f6f6f6;
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0; 
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%; }
 
+      table {
+        border-collapse: separate;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        width: 100%; }
+        table td {
+          font-family: sans-serif;
+          font-size: 14px;
+          vertical-align: top; }
 
-</tbody>
-</table>
-</td>
-</tr>
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
 
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
-</td>
-</tr>
-</tbody>
-</table>
+      .body {
+        background-color: #f6f6f6;
+        width: 100%; }
 
-<br>
-</td>
-</tr>
-</tbody>
-</table>
-</center>
-</div>
-</div>
-<br>
-<p style="font-family:Calibri;font-size:7pt;color:#000000;margin:15pt;" align="Left">
-C2 General<br>
-</p>
-</body>
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .container {
+        display: block;
+        Margin: 0 auto !important;
+        /* makes it centered */
+        width: 980px; 
+        padding: 10px;
+        width: auto !important;
+        width: 580px; }
+
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .content {
+        box-sizing: border-box;
+        display: block;
+        Margin: 0 auto;
+        width: 980px;
+        padding: 10px; }
+
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #fff;
+        border-radius: 3px;
+        width: 100%; }
+
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px; }
+
+      .footer {
+        clear: both;
+        padding-top: 10px;
+        text-align: center;
+        width: 100%; }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center; }
+
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      h1,
+      h2,
+      h3,
+      h4 {
+        color: #000000;
+        font-family: sans-serif;
+        font-weight: 400;
+        line-height: 1.4;
+        margin: 0;
+        Margin-bottom: 30px; }
+
+      h1 {
+        font-size: 35px;
+        font-weight: 300;
+        text-align: center;
+        text-transform: capitalize; }
+
+      p,
+      ul,
+      ol {
+        font-family: sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        margin: 0;
+        Margin-bottom: 15px; }
+        p li,
+        ul li,
+        ol li {
+          list-style-position: inside;
+          margin-left: 5px; }
+
+      a {
+        color: #3498db;
+        text-decoration: underline; }
+
+      /* -------------------------------------
+          BUTTONS
+      ------------------------------------- */
+      .btn {
+        box-sizing: border-box;
+        width: 100%; }
+        .btn > tbody > tr > td {
+          padding-bottom: 15px; }
+        .btn table {
+          width: auto; }
+        .btn table td {
+          background-color: #ffffff;
+          border-radius: 5px;
+          text-align: center; }
+        .btn a {
+          background-color: #ffffff;
+          border: solid 1px #3498db;
+          border-radius: 5px;
+          box-sizing: border-box;
+          color: #3498db;
+          cursor: pointer;
+          display: inline-block;
+          font-size: 14px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 25px;
+          text-decoration: none;
+          text-transform: capitalize; }
+
+      .btn-primary table td {
+        background-color: #3498db; }
+
+      .btn-primary a {
+        background-color: #3498db;
+        border-color: #3498db;
+        color: #ffffff; }
+
+      /* -------------------------------------
+          OTHER STYLES THAT MIGHT BE USEFUL
+      ------------------------------------- */
+      .last {
+        margin-bottom: 0; }
+
+      .first {
+        margin-top: 0; }
+
+      .align-center {
+        text-align: center; }
+
+      .align-right {
+        text-align: right; }
+
+      .align-left {
+        text-align: left; }
+
+      .clear {
+        clear: both; }
+
+      .mt0 {
+        margin-top: 0; }
+
+      .mb0 {
+        margin-bottom: 0; }
+
+      .preheader {
+        color: transparent;
+        display: none;
+        height: 0;
+        max-height: 0;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        mso-hide: all;
+        visibility: hidden;
+        width: 0; }
+
+      .powered-by a {
+        text-decoration: none; }
+
+      hr {
+        border: 0;
+        border-bottom: 1px solid #f6f6f6;
+        Margin: 20px 0; }
+
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+        table[class=body] h1 {
+          font-size: 28px !important;
+          margin-bottom: 10px !important; }
+        table[class=body] p,
+        table[class=body] ul,
+        table[class=body] ol,
+        table[class=body] td,
+        table[class=body] span,
+        table[class=body] a {
+          font-size: 16px !important; }
+        table[class=body] .wrapper,
+        table[class=body] .article {
+          padding: 10px !important; }
+        table[class=body] .content {
+          padding: 0 !important; }
+        table[class=body] .container {
+          padding: 0 !important;
+          width: 100% !important; }
+        table[class=body] .main {
+          border-left-width: 0 !important;
+          border-radius: 0 !important;
+          border-right-width: 0 !important; }
+        table[class=body] .btn table {
+          width: 100% !important; }
+        table[class=body] .btn a {
+          width: 100% !important; }
+        table[class=body] .img-responsive {
+          height: auto !important;
+          max-width: 100% !important;
+          width: auto !important; }}
+
+      /* -------------------------------------
+          PRESERVE THESE STYLES IN THE HEAD
+      ------------------------------------- */
+      @media all {
+        .ExternalClass {
+          width: 100%; }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+          line-height: 100%; }
+        .apple-link a {
+          color: inherit !important;
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          text-decoration: none !important; } 
+        .btn-primary table td:hover {
+          background-color: #34495e !important; }
+        .btn-primary a:hover {
+          background-color: #34495e !important;
+          border-color: #34495e !important; } }
+
+    </style>
+  </head>
+  <body class="">
+    <table border="0" cellpadding="0" cellspacing="0" class="body" height="100%" width="100%">
+      <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+          <div class="content">
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            
+            <div style="background: #000; border-bottom: 1px solid #FC561F; height:80px; padding:2%">
+              <span style="color:#fff; font-size:30px">_VSSI CLOUD</span>
+            </div>
+
+         
+
+            <table class="main" height="100%" width="100%">
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class="wrapper">
+                  <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%">
+                 
+                    <tr>
+                      <td>
+                        <p>Hi,</p>
+
+                        <table class="table table-bordered">
+                            <tbody>
+                              <tr>
+                                <td>VM Name</td>
+                                <td>{{$vm->name}}</td>
+                              </tr>
+                              <tr>
+                                <td>UserName</td>
+                                <td>{{$vm->username}}</td>
+                              </tr>
+                              <tr>
+                                <td>Password</td>
+                                <td>{{$vm->pass}}</td>
+                              </tr>
+                             
+
+                            </tbody>
+                          </table>
+                        
+                        <br>
+                        <p>Regards</p>
+                        <p>Cloud Team</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+              <!-- END MAIN CONTENT AREA -->
+              </table>
+
+            
+<!-- END CENTERED WHITE CONTAINER --></div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
 </html>
