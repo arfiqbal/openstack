b79c02d942ea79130cc56a8e673a68e34bbb7f80
arif <bickkuarif@gmail.com>
2020-04-09 13:32:41 +0530

    ansible v3

--- public/template/template.tf
+++ public/template/template.tf
@@ -38,7 +38,7 @@ resource "openstack_compute_instance_v2" "vm" {
 
   flavor_id       = var.flavor
   key_pair        = "vdf-key1"
-  security_groups = ["all-open"]
+  security_groups = ["default"]
   user_data = <<EOF
   #cloud-config
   package_upgrade: true
