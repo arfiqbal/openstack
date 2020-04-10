7bae053a63d0cfab92d6e226c58c9ea2a5d39583
arif <bickkuarif@gmail.com>
2020-04-10 10:58:33 +0530

    pam file v1

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -88,7 +88,7 @@ class VmController extends Controller
         $image = $service->getImage($app->uid);
 
         
-        dd($image);
+        dd($image->retrieve());
 
         //dd($request->toArray());
         ini_set('max_execution_time', 3600);
