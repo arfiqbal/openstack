3bf682ce2ae9c7ba2ef1734c62ed093c5b40c3e9
arif <bickkuarif@gmail.com>
2020-04-10 20:57:55 +0530

    pam file v1

--- public/template/template.tf
+++ public/template/template.tf
@@ -41,12 +41,13 @@ resource "openstack_compute_instance_v2" "vm" {
   security_groups = ["all-open"]
   user_data = <<EOF
   #cloud-config
+  runcmd:
+   - [ sh, -c, "hostnamectl set-hostname ${var.hostname}" ]
+   - [ sh, -c , "echo nameserver 10.85.50.19 > /etc/resolv.conf"]
   package_upgrade: true
   packages:
    - freeipa-client
   runcmd:
-   - [ sh, -c, "hostnamectl set-hostname ${var.hostname}" ]
-   - [ sh, -c , "echo nameserver 10.85.50.19 > /etc/resolv.conf"]
    - [ sh, -c, "ipa-client-install --mkhomedir -p arif@CLOUD.VSSI.COM -w 'redhat12' --server=inidmor1.cloud.vssi.com --domain cloud.vssi.com -U"]
   write_files:
   - path: /etc/pam.d/common-session
