5e2c0bb00883d369c1c7f22e6183fd4f76a68865
arif <bickkuarif@gmail.com>
2020-04-14 20:07:55 +0530

    image

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -68,7 +68,7 @@ class VmController extends Controller
         $images = $servers->imagesV2()->listImages();
 
         foreach ($images as $image) {
-            dd($image);
+            dd($image->id);
         }
 
         dd('end');
