686ae146684219f2b9281472664634fa48503976
arif <bickkuarif@gmail.com>
2020-04-14 16:41:05 +0530

    new admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
                     <div class="container">
                         
                         <div class="row justify-content-center">
-                            <div class="col-lg-5">
+                            <div class="col-lg-6">
                               
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header">
