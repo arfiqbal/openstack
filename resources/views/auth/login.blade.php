efe1d46962f132d7aadd0b6c4a816f7c6056bbd3
arif <bickkuarif@gmail.com>
2020-04-14 16:14:35 +0530

    new admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,6 +9,32 @@
         <title>VSSI CLOUD - Admin Login</title>
         <link href="{{ asset('css/styles.css')}}" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+        <style>
+            svg {
+                font-family: 'Russo One', sans-serif;
+                position: absolute; 
+                width: 100%; height: 100%;
+            }
+            svg text {
+                text-transform: uppercase;
+                animation: stroke 5s infinite alternate;
+                stroke-width: 2;
+                stroke: #365fa0;
+                font-size: 140px;
+            }
+            @keyframes stroke {
+                0%   {
+                    fill: rgba(72,138,20,0); stroke: rgba(54,95,160,1);
+                    stroke-dashoffset: 25%; stroke-dasharray: 0 50%; stroke-width: 2;
+                }
+                70%  {fill: rgba(72,138,20,0); stroke: rgba(54,95,160,1); }
+                80%  {fill: rgba(72,138,20,0); stroke: rgba(54,95,160,1); stroke-width: 3; }
+                100% {
+                    fill: rgba(72,138,204,1); stroke: rgba(54,95,160,0); 
+                    stroke-dashoffset: -25%; stroke-dasharray: 50% 0; stroke-width: 0;
+                }
+            }
+        </style>
     </head>
     <body class="bg-primary">
         <div id="layoutAuthentication">
@@ -17,6 +43,11 @@
                     <div class="container">
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
+                                <svg viewBox="0 0 1320 300">
+                                    <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+                                        VSSI CLOUD
+                                    </text>
+                                </svg>	
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                     <div class="card-header">
                                         <h3 class="text-center font-weight-light my-4">{{ __('Login') }}</h3>
