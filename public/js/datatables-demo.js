7b3a6704a6ad69cc397b4af32496ab9ad2fa385b
arif <bickkuarif@gmail.com>
2020-04-14 17:40:32 +0530

    new admin

--- public/js/datatables-demo.js
+++ public/js/datatables-demo.js
@@ -1,4 +1,24 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('.dataTable').DataTable();
+  //$('.dataTable').DataTable();
+  $('.dataTable thead th').each( function () {
+    var title = $(this).text();
+    $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+} );
+
+// DataTable
+var table = $('.dataTable').DataTable();
+
+// Apply the search
+table.columns().every( function () {
+    var that = this;
+
+    $( 'input', this.footer() ).on( 'keyup change clear', function () {
+        if ( that.search() !== this.value ) {
+            that
+                .search( this.value )
+                .draw();
+        }
+    } );
+} );
 });
