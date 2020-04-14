a399a8525ad9cdc589d8e5be5f66538745871b02
arif <bickkuarif@gmail.com>
2020-04-14 21:47:54 +0530

    image

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -67,7 +67,7 @@ class VmController extends Controller
         $servers = $this->openstack->defaultAuthentication();
         $images = $servers->imagesV2()->listImages();
         //['id' => '6bf51948-a5b8-430a-96a2-b2d12fd8b546']
-        $compute = $servers->computeV2();
+        $compute = $servers->computeV2(['id' => '6bf51948-a5b8-430a-96a2-b2d12fd8b546']);
 
         $flavor = $compute->getFlavor(['id' => '1a4741dc-5a07-4126-977e-73f62ef15f40']);
 
