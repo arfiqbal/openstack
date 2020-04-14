b917ddf069438b49fb9d8f97076f679690a8f5ba
arif <bickkuarif@gmail.com>
2020-04-14 22:18:38 +0530

    image

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -71,7 +71,7 @@ class VmController extends Controller
         // $server = $compute->getServer(['id' => '6bf51948-a5b8-430a-96a2-b2d12fd8b546','flavor' => ""], true);
         // dd($server);
 
-        $flavor = $compute->getFlavor(['id' => '1a4741dc-5a07-4126-977e-73f62ef15f40','ram' =>'','vcpus' => ''], true);
+        $flavor = $compute->getFlavor(['id' => '1a4741dc-5a07-4126-977e-73f62ef15f40'], ['ram']);
         dd($flavor);
         dd($flavor->retrieve());
         dd('tex');
