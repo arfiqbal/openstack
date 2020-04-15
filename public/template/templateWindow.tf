4e346b679ec4350c66567199d7b147b4529065a0
arif <bickkuarif@gmail.com>
2020-04-15 15:32:50 +0530

    rework

--- public/template/templateWindow.tf
+++ public/template/templateWindow.tf
@@ -39,14 +39,8 @@ resource "openstack_compute_instance_v2" "vm" {
 
 
   flavor_id       = var.flavor
-  key_pair        = "vdf-key1"
   security_groups = ["all-open"]
-  user_data = <<EOF
-  #cloud-config
-  hostname: ${var.hostname}
-  fqdn: ${var.hostname}
-EOF
-
+  
 
   metadata = {
        email = var.emailid
