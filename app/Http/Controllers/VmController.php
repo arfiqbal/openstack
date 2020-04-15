8bca3c288ab9923aa7e6cad4815e0815f3d3c352
arif <bickkuarif@gmail.com>
2020-04-15 08:18:28 +0530

    rework

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -460,6 +460,6 @@ class VmController extends Controller
 
         return view('recreateVM',
         ['apps' => $apps, 
-        'flavors' => $flavors, 'lastVm' => $vmDetail]);
+        'flavors' => $flavors, 'vmDetail' => $vmDetail]);
     }
 }
