b732130d1c1b8fd9713b981370ff8cce0ecfa4ef
arif <bickkuarif@gmail.com>
2020-04-15 14:02:46 +0530

    rework

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Cloud-Poartal {{date('Y')}}</div>
+            <div class="text-muted">Copyright &copy; Cloud-Portal {{date('Y')}}</div>
             
         </div>
     </div>
