141110db1a99a6ae074fd4d0ab7e65a9e45a1cbf
arif <bickkuarif@gmail.com>
2020-04-16 07:57:35 +0530

    rework

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -72,7 +72,7 @@ class VmController extends Controller
         //dd($allVM->toArray());
         $flavor = $compute->getFlavor(['id' => $allVM->flavor]);
         $flavor->retrieve();
-        echo $flavor->ram;
+        dd( "RAM = '.$flavor->ram.'  vCPU ='.$flavor->vcpus");
         dd($flavor);
         
 
