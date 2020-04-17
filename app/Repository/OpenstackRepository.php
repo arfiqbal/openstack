d216de9ba5886ccc7922a0d31e606a3a2789007b
arif <bickkuarif@gmail.com>
2020-04-17 12:21:52 +0530

    vm detail

--- app/Repository/OpenstackRepository.php
+++ app/Repository/OpenstackRepository.php
@@ -162,12 +162,14 @@ class OpenstackRepository
     public function changeServerFlavor($vmuid,$flavor)
     {
         $vm = VM::with('application')->find($vmuid);
-        dd($vm->toArray());
+       
         $servers = $this->defaultAuthentication();
         $compute = $servers->computeV2();
         $server = $compute->getServer([
             'id' => $vm->vm_uid,
         ]);
+        $server->retrieve();
+        dd($server);
         $server->resize($flavor);
         
 
