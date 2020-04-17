0f5a0223534c7a0ada074a4cfb83a3c212cd68ab
arif <bickkuarif@gmail.com>
2020-04-17 09:37:05 +0530

    vm detail

--- resources/views/show-server.blade.php
+++ resources/views/show-server.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-  <h1 class="mt-4">{{$serverDetail->name}} Server Detail</h1>
+  <h1 class="mt-4">{{$serverDetail->jira}} Server Detail</h1>
   
   <div class="row">
     <div class="card mb-4">
@@ -15,6 +15,63 @@
         <a class="btn btn-success pull-right"  href=""><i class="fas fa-text-height"></i></a>
       </div>
       <div class="card-body">
+        <table class="table table-borderless">
+          
+          <tbody>
+            <tr>
+              <th scope="row">Server Name</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Jira Ticket</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Hostname</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">UserName</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Email</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Server OS</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Server UID</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Nic1</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Nic2</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Flavor</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Flavor</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Created By</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">Created At</th>
+              <td>Mark</td>
+            </tr>
+          </tbody>
+        </table>
 
         
       </div>
