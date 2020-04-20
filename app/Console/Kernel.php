4a8f6f4c1e6748146b18759aa6ae7c583b3318e3
arif <bickkuarif@gmail.com>
2020-04-20 12:03:33 +0530

    application cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('generate:report')
-        ->everyMinute();
+        ->weekly()->mondays()->at('11:59');
     }
 
     /**
