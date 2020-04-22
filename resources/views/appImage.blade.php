51b7e184bda44d640b9361ebba0958e3e804e38f
arif <bickkuarif@gmail.com>
2020-04-22 16:56:53 +0530

    report updated

--- resources/views/appImage.blade.php
+++ resources/views/appImage.blade.php
@@ -11,7 +11,7 @@ Add Application | VSSI Cloud
       <li class="breadcrumb-item active">App</li>
   </ol>
   @if (session('status'))
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
       {{ session('status') }} has been added successfully
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
         <span aria-hidden="true">&times;</span>
