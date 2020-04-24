8e936ae9cadc1aa0a392995752d410470df01769
arif <bickkuarif@gmail.com>
2020-04-24 16:40:54 +0530

    vm name

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -57,6 +57,8 @@ class ImageController extends Controller
         $imageSplit = explode('?',$request->image);
 
         $chkImage = Application::where('uid','$imageSplit[0]')->get();
+        dd($chkImage->toArray());
+        
         if(count($chkImage)){
             return redirect()->route('addImage')->with('status', ' Image Already Exists');
         }else{
