a85a9f768ae7f8702ed69cf1d115c4b6aeb89265
arif <bickkuarif@gmail.com>
2020-05-13 09:15:36 +0530

    ldap

--- app/Ldap/User.php
+++ app/Ldap/User.php
@@ -6,11 +6,18 @@ use LdapRecord\Models\Model;
 
 class User extends Model
 {
-    //protected $guidKey = 'entryuuid';
+    protected $guidKey = 'dn';
     /**
      * The object classes of the LDAP model.
      *
      * @var array
      */
-    public static $objectClasses = [];
+    public static $objectClasses = [
+        'top',
+        'person',
+        'organizationalperson',
+        'inetorgperson',
+        'inetuser',
+        'posixaccount'
+    ];
 }
