2f072b421fb81e91ad9dac05e28e2ea552472916
arif <bickkuarif@gmail.com>
2020-06-08 13:45:59 +0530

    snapshot v1

--- app/Http/Controllers/VolumeController.php
+++ app/Http/Controllers/VolumeController.php
@@ -33,10 +33,13 @@ class VolumeController extends Controller
     {
         $servers = $this->openstack->openstackProjectID('198f0660ae894f87a5ad2522e6dec551');
         $service = $servers->blockStorageV2();
-        $snaps = $service->listSnapshots(false,[
-            'volumeId'     => 'dfb43258-283e-4245-8658-c39fc5782cdd'
-        ]);
+        // $snaps = $service->listSnapshots(false,[
+        //     'volumeId'     => 'dfb43258-283e-4245-8658-c39fc5782cdd'
+        // ]);
 
+        $snaps = $service->getVolume('dfb43258-283e-4245-8658-c39fc5782cdd');
+        $snaps->retrieve();
+        dd($snaps);
         //$snaps = $service->listSnapshots();
 
         foreach ($snaps as $snap) {
