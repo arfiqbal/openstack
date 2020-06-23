5885658927167868fdf41bb4490ce77a8af6445f
arif <bickkuarif@gmail.com>
2020-06-23 11:40:48 +0530

    port testing

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -69,7 +69,7 @@ class VmController extends Controller
 
         $ports = $networking->listPorts();
         foreach($ports as $port){
-            var_dump($port['fixedIps']);
+            var_dump($port->fixedIps);
         }
         dd('all port');
 
