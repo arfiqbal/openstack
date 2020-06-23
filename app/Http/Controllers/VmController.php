7d41f3a576801474e8db4b41b574363a90773e57
arif <bickkuarif@gmail.com>
2020-06-23 14:40:13 +0530

    port testing

--- app/Http/Controllers/VmController.php
+++ app/Http/Controllers/VmController.php
@@ -68,7 +68,7 @@ class VmController extends Controller
         $networking = $projectsServer->networkingV2();
 
         $ports = $networking->listPorts([
-            'status' => 'DOWN',
+            'networkId' => 'd5c2c4ab-64f8-4dde-82bb-9c0b65a7f5d8',
         ]);
         foreach($ports as $port){
             //var_dump($port->fixedIps[0]['ip_address']);
