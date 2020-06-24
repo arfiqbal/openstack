002154837cb742f0c4fd006bb98a2953d2a639fa
arif <bickkuarif@gmail.com>
2020-06-24 16:53:55 +0530

    vol

--- app/Http/Controllers/VolumeController.php
+++ app/Http/Controllers/VolumeController.php
@@ -35,7 +35,7 @@ class VolumeController extends Controller
         $servers = $this->openstack->defaultAuthentication();
         $service = $servers->blockStorageV2();
         $volume = $service->getVolume('eb788ac6-d3b9-42b0-b566-ac53cab5a56c');
-        dd($volume);
+        dd($volume->id);
         $snaps = $service->listSnapshots(true,[
             'volumeId'=> $volume
             ]);
