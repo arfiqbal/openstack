2c1c20d3d123a146da6f626fcb4261e49a42aa35
arif <bickkuarif@gmail.com>
2020-06-24 18:50:54 +0530

    vol

--- app/Http/Controllers/VolumeController.php
+++ app/Http/Controllers/VolumeController.php
@@ -31,7 +31,7 @@ class VolumeController extends Controller
      */
     public function index()
     {
-        $list = $this->openstack->listSnapshot('198f0660ae894f87a5ad2522e6dec551', 'eb788ac6-d3b9-42b0-b566-ac53cab5a56c');
+        $list = $this->openstack->listSnapshot('4d9031e2761c482e873ee7fcdf73ba29', 'eb788ac6-d3b9-42b0-b566-ac53cab5a56c');
         dd($list);
         //$servers = $this->openstack->openstackProjectID('198f0660ae894f87a5ad2522e6dec551');
         $servers = $this->openstack->defaultAuthentication();
